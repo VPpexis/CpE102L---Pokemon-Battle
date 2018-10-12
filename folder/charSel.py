57ff48c6390acc7d51baea264fd2aecf8e130870
Van Philip Panugan <panuganvanphilip@gmail.com>
2018-10-12 09:29:37 +0800

    Add files via upload

---
+++ folder/charSel.py
@@ -0,0 +1,27 @@
+import csv
+
+class charSel():
+  def __str__(self):
+    player = ""
+    print("Pick your Pokemon Character" "\n")
+    filename = "folder/charac.txt"
+    file = open(filename, "r")
+    print(file.read())
+    file.close()
+
+
+    with open( "folder/charac.txt","r") as f:
+      characReader=csv.reader(f)
+      id=input("\n" "Enter Pokemon ID (format:XXX)  :")
+      for row in characReader:
+        for field in row:
+          if field==id:
+       
+            currentindex=row.index(id)
+            player = str(row[currentindex+1])
+            print("Your Chosen Pokemon Character: " + player)
+
+    
+    return player
+
+        
