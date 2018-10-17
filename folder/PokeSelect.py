7c5126b78f33a542d44c348b6a7c3f2f59c92b1d
Van Philip Panugan <panuganvanphilip@gmail.com>
2018-10-17 17:23:31 +0800

    Update PokeSelect.py

--- folder/PokeSelect.py
+++ folder/PokeSelect.py
@@ -1,20 +1,51 @@
 import csv
 
-    print("Pick your Pokemon Character" "\n")
+class PokeSelect():
+  def PokeSelect():
+    checker = True
+    data = ["","","","",""]
+    print("#############################################")
+    print("Pick your Pokemon Character")
+    print("ID:     Name:")
     filename = "folder/charac.txt"
     file = open(filename, "r")
-    print(file.read())
+    characReader=csv.reader(file)
+    for column in characReader:
+        a = ""
+        b = ""
+        a = column[0]
+        b = column[1]
+        print("%s     %s" % (a,b))
+
+
     file.close()
+    print("=============================================")
 
+    while checker:
+      #Opens and Display
+      with open( "folder/charac.txt","r") as f:
+        characReader=csv.reader(f)
+        id=input("Enter Pokemon ID (format:XXX)  :")
 
-    with open( "folder/charac.txt","r") as f:
-      characReader=csv.reader(f)
-      id=input("\n" "Enter Pokemon ID (format:XXX)  :")
-      for row in characReader:
-        for field in row:
-          if field==id:
-       
-            currentindex=row.index(id)
-            player = str(row[currentindex+1])
-            print("Your Chosen Pokemon Character: " + player)
+        #Opens and Finds
+        for row in characReader:
+          for field in row:
+            if field==id:
+              currentindex=row.index(id)
+              data[0] = str(row[currentindex+1])
+              data[1] = str(row[currentindex +2])
+              data[2] = str(row[currentindex +3])
+              data[3] = str(row[currentindex + 4])
+              data[4] = str(row[currentindex + 5])
+              print("#############################################")
+              print("Your Chosen Pokemon Character: " + data[0])
+              print("Attributes: \n Att: %s \n Def: %s \n Agt: %s" % (data[1], data[2], data[3]))
 
+      if(data[0] == ""):
+        checker = True
+        print("Input not Recognized. Pls Try Again")
+      else:
+        checker = False
+      
+    print("#############################################")
+    return data
